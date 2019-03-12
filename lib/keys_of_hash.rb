2fe187f0c819d1447860d9f6a61952567ce47cd4
Danny Wakeling <wakeling101@hotmail.com>
2019-03-12 09:44:28 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,3 +3,8 @@ class Hash
     map {|key, value| args.include?(value) ? key : nil }.compact
   end
 end
+
+#map returns an array of results
+#if arguments include the value
+#then display key else display nil
+#.compact removes nil's
